01fbad55ba4be88da5cbb6c919badc6a771740e4
Anil Antony <anilvithayathil007@gmail.com>
2024-04-19 11:22:11 +0530

    fast laugh UI

--- lib/presentation/fast_laugh/Screen_fast_laugh.dart
+++ lib/presentation/fast_laugh/Screen_fast_laugh.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/presentation/fast_laugh/widget/video_list_item.dart';
 
 class ScreenFastLaugh extends StatelessWidget {
-  const ScreenFastLaugh({super.key});
+  const ScreenFastLaugh({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+            child: PageView(
+                scrollDirection: Axis.vertical,
+                children: List.generate(state.videosList.length, (index) {
+                  return VideoListItemInheritedWidget(
+                    widget: VideoListItem(
+                      key: Key(index.toString()),
+                      index: index,
+                    ),
+                    movieData: state.videosList[index],
+                  );
+                }))));
   }
 }
